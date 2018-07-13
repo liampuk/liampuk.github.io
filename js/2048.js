d0c79261515d49daf1226ea8981f22cc216d63f5
liampuk <liamp.uk@gmail.com>
2018-07-13 08:44:52 +0100

    Update 2048.js

--- js/2048.js
+++ js/2048.js
@@ -315,49 +315,44 @@ function calculateHighScore(){
     document.getElementById("highScore").innerHTML = "High Score: " + higherScore;
 }
 
-//  stackoverflow code
+document.addEventListener('touchstart', touchDetect, false);
+document.addEventListener('touchmove', swipeDetect, false);
 
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
+var xDown, yDown;
 
-var xDown = null;                                                        
-var yDown = null;                                                        
+function touchDetect(evt){
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+}
 
-function handleTouchStart(evt) {                                         
-    xDown = evt.touches[0].clientX;                                      
-    yDown = evt.touches[0].clientY;                                      
-};                                                
-
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
+function swipeDetect(evt){
+    if ( xDown == null || yDown == null ) {
         return;
     }
-
-    var xUp = evt.touches[0].clientX;                                    
+    var xUp = evt.touches[0].clientX;
     var yUp = evt.touches[0].clientY;
 
     var xDiff = xDown - xUp;
     var yDiff = yDown - yUp;
 
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            moveLeft(); 
-        } else {
+    if(Math.abs(xDiff) > Math.abs(yDiff)){
+        if(xDiff > 0){
+            moveLeft();
+        }else{
             moveRight();
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
+        }
+    }else{
+        if(yDiff > 0){
             moveUp();
-        } else { 
+        }else{
             moveDown();
-        }                                                                 
+        }
     }
-    checkEnd();
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
 
-// stack overflow code end
+    xDown = null;
+    yDown = null;
+
+    checkEnd();
+}
 
 window.onload = setupGame();
