70f845f401cf5aaa8f6df64f2d3ec1249cd6eec6
liampuk <liampuk@gmail.com>
2018-09-25 21:48:43 +0100

    emojiPad v0.3

--- js/emoji.js
+++ js/emoji.js
@@ -1,5 +1,5 @@
 // var emojis = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var emojis = ["\uD83D\uDE02","\uD83D\uDE05","\uD83D\uDE0B","\uD83D\uDE0E","\uD83D\uDE0D","\uD83E\uDD14","\uD83D\uDE36","\uD83D\uDE34","\uD83D\uDE2D","\uD83D\uDE2C","\uD83D\uDE31","\uD83D\uDE35","\uD83D\uDE20","\uD83D\uDC4C","\uD83D\uDD25","\u2764"];
+var emojis = ["\uD83D\uDE02","\uD83D\uDE05","\uD83D\uDE0B","\uD83D\uDE0E","\uD83D\uDE0D","\uD83E\uDD14","\uD83D\uDE36","\uD83D\uDE34","\uD83D\uDE2D","\uD83D\uDE2C","\uD83D\uDE31","\uD83D\uDE35","\uD83D\uDE20","\uD83D\uDC4C","\uD83D\uDD25","\u2764\uFE0F"];
 
 function renderPad(){
     var width = Math.ceil(Math.sqrt(emojis.length));
@@ -34,6 +34,21 @@ function flashToast(emoji){
     setTimeout(function(){
         toast.style.opacity = 0;
     }, 1500);
+}
+
+function toggleTheme(day){
+    if(day){
+        document.body.style.color = "black";
+        document.body.style.backgroundColor = "white";
+        document.getElementById("sun").innerHTML = '<i class="fas fa-circle"></i>';
+        document.getElementById("moon").innerHTML = '<i class="far fa-moon"></i>';
+    }else{
+        document.body.style.color = "white";
+        document.body.style.backgroundColor = "black";
+        document.getElementById("sun").innerHTML = '<i class="far fa-circle"></i>';
+        document.getElementById("moon").innerHTML = '<i class="fas fa-moon"></i>';
+    }
+
 }
 
 window.onload = function(){
