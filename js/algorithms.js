fae9e58a2b0294423dc668a7134c16ccc7a93d40
liampuk <liampuk@gmail.com>
2018-11-17 19:39:00 +0000

    algorithms css

--- js/algorithms.js
+++ js/algorithms.js
@@ -1,19 +1,18 @@
 var screen;
-prevElem = null;
-screenState = false;
+var prevElem = null;
+var screenState = false;
+var dark = false;
 
 function toggleDisplay(elem){
     if(prevElem != null && elem == prevElem && screenState){
-        prevElem.style.backgroundColor = "transparent";
-        prevElem.style.color = "black";
+        prevElem.classList.remove("active");
         screen.style.opacity = "0";
         screen.style.transform = "scale(.98)";
         screenState = false;
     }else{
         if(prevElem != null){
             document.getElementById(prevElem.innerText.substring(2).replace(new RegExp(" ", "g"), '')).setAttribute('style', 'display:none !important');
-            prevElem.style.backgroundColor = "transparent";
-            prevElem.style.color = "black";
+            prevElem.classList.remove("active");
         }
         if(!screenState){
             screenState = true;
@@ -21,10 +20,35 @@ function toggleDisplay(elem){
             screen.style.transform = "scale(1)";
         }
         document.getElementById(elem.innerText.substring(2).replace(new RegExp(" ", "g"), '')).setAttribute('style', 'display:block !important');
-        elem.style.backgroundColor = "#29B6F6";
-        elem.style.color = "white";
+        elem.classList.add("active");
     }
     prevElem = elem;
+}
+
+function changeTheme(){
+    if(dark){
+        document.documentElement.classList.remove("dark-scroll");
+        document.body.classList.remove("body-dark");
+        document.getElementById("colours").classList.remove("colours-dark");
+        document.getElementById("colours").classList.add("colours-light");
+        dark = false;
+        var accents = document.getElementsByClassName("accent-dark");
+        while(accents.length > 0){
+            accents[0].classList.add("accent");
+            accents[0].classList.remove("accent-dark");
+        }
+    }else{
+        document.documentElement.classList.add("dark-scroll");
+        document.body.classList.add("body-dark");
+        document.getElementById("colours").classList.add("colours-dark");
+        document.getElementById("colours").classList.remove("colours-light");
+        dark = true;
+        var accents = document.getElementsByClassName("accent");
+        while(accents.length > 0){
+            accents[0].classList.add("accent-dark");
+            accents[0].classList.remove("accent");
+        }
+    }
 }
 
 function init(){
@@ -33,11 +57,9 @@ function init(){
     setTimeout(function(){
         elem.click();
         setTimeout(function(){
-            elem.style.backgroundColor = "transparent";
-            elem.style.color = "black";
+            elem.classList.remove("active");
             setTimeout(function(){
-                elem.style.backgroundColor = "#29B6F6";
-                elem.style.color = "white";
+                elem.classList.add("active");
             }, 100);
         }, 200);
     }, 500);
