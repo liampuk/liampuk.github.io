439db25e6e2504994234dc90a2bc78ee6bbdff50
liampuk <liampuk@gmail.com>
2018-11-17 20:36:39 +0000

    algo css

--- js/algorithms.js
+++ js/algorithms.js
@@ -3,18 +3,18 @@ var prevElem = null;
 var screenState = false;
 var dark = false;
 
-function toggleDisplay(elem){
-    if(prevElem != null && elem == prevElem && screenState){
+function toggleDisplay(elem) {
+    if (prevElem != null && elem == prevElem && screenState) {
         prevElem.classList.remove("active");
         screen.style.opacity = "0";
         screen.style.transform = "scale(.98)";
         screenState = false;
-    }else{
-        if(prevElem != null){
+    } else {
+        if (prevElem != null) {
             document.getElementById(prevElem.innerText.substring(2).replace(new RegExp(" ", "g"), '')).setAttribute('style', 'display:none !important');
             prevElem.classList.remove("active");
         }
-        if(!screenState){
+        if (!screenState) {
             screenState = true;
             screen.style.opacity = "1";
             screen.style.transform = "scale(1)";
@@ -25,40 +25,40 @@ function toggleDisplay(elem){
     prevElem = elem;
 }
 
-function changeTheme(){
-    if(dark){
+function changeTheme() {
+    if (dark) {
         document.documentElement.classList.remove("dark-scroll");
         document.body.classList.remove("body-dark");
         document.getElementById("colours").classList.remove("colours-dark");
         document.getElementById("colours").classList.add("colours-light");
         dark = false;
         var accents = document.getElementsByClassName("accent-dark");
-        while(accents.length > 0){
+        while (accents.length > 0) {
             accents[0].classList.add("accent");
             accents[0].classList.remove("accent-dark");
         }
-    }else{
+    } else {
         document.documentElement.classList.add("dark-scroll");
         document.body.classList.add("body-dark");
         document.getElementById("colours").classList.add("colours-dark");
         document.getElementById("colours").classList.remove("colours-light");
         dark = true;
         var accents = document.getElementsByClassName("accent");
-        while(accents.length > 0){
+        while (accents.length > 0) {
             accents[0].classList.add("accent-dark");
             accents[0].classList.remove("accent");
         }
     }
 }
 
-function init(){
+function init() {
     screen = document.getElementById("screen");
     var elem = document.getElementsByTagName("span")[4];
-    setTimeout(function(){
+    setTimeout(function () {
         elem.click();
-        setTimeout(function(){
+        setTimeout(function () {
             elem.classList.remove("active");
-            setTimeout(function(){
+            setTimeout(function () {
                 elem.classList.add("active");
             }, 100);
         }, 200);
