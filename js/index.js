2adf899abee24addedd8724dcabfe2fb61a7317d
liampuk <liamp.uk@gmail.com>
2020-01-10 00:39:10 +0000

    updated design for 2020

--- js/index.js
+++ js/index.js
@@ -1,23 +1,193 @@
-var page;
-asp = 1.25;
+var ctx;
+var width;
+var height;
+var ctr = 0;
+var colBool = 0;
+var shift = 0;
+var speed = 1.5;
 
-function resize() {
-    w = window.innerWidth;
-    h = window.innerHeight;
-    if (h > w * asp) {
-        page.style.width = "85vw";
-        page.style.height = "106.25vw";
-    } else {
-        page.style.width = "68vh";
-        page.style.height = "85vh";
+var col1 = Math.floor(Math.random()*360)+",75%,55%";
+var col2 = Math.floor(Math.random()*360)+",75%,55%";
+// col2 = "131, 49, 238";
+var col = col1;
+
+var wormX;
+var wormY;
+var wormXvel = speed;
+var wormYvel = speed;
+
+var mod = 0;
+
+var Asciimap = "               ,_   .  ._. _.  .                                     \r\n           , _-\\\',\'|~\\~      ~\/      ;-\'_   _-\'     ,;_;_,    ~~-    \r\n  \/~~-\\_\/-\'~\'--\' \\~~| \',    ,\'      \/  \/ ~|-_\\_\/~\/~      ~~--~~~~\'--_\r\n  \/              ,\/\'-\/~ \'\\ ,\' _  , \'|,\'|~                   ._\/-, \/~ \r\n  ~\/-\'~\\_,       \'-,| \'|. \'   ~  ,\\ \/\'~                \/    \/_  \/~   \r\n.-~      \'|        \'\',\\~|\\       _\\~     ,_  ,               \/|      \r\n          \'\\        \/\'~          |_\/~\\\\,-,~  \\ \"         ,_,\/ |      \r\n           |       \/            ._-~\'\\_ _~|              \\ ) \/       \r\n            \\   __-\\           \'\/      ~ |\\  \\_          \/  ~        \r\n  .,         \'\\ |,  ~-_      - |          \\\\_\' ~|  \/\\  \\~ ,          \r\n               ~-_\'  _;       \'\\           \'-,   \\,\' \/\\\/  |          \r\n                 \'\\_,~\'\\_       \\_ _,       \/\'    \'  |, \/|\'          \r\n                   \/     \\_       ~ |      \/         \\  ~\'; -,_.     \r\n                   |       ~\\        |    |  ,        \'-_, ,; ~ ~\\   \r\n                    \\,      \/        \\    \/ \/|            ,-, ,   -, \r\n                     |    ,\/          |  |\' |\/          ,-   ~ \\   \'.\r\n                    ,|   ,\/           \\ ,\/              \\       |    \r\n                    \/    |             ~                 -~~-, \/   _ \r\n                    |  ,-\'                                    ~    \/ \r\n                    \/ ,\'                                      ~      \r\n                    \',|  ~                                           \r\n                      ~\'                                             ";
+
+function rndColours(){
+    var hue1 = Math.floor(Math.random()*360);
+    var hue2 = Math.floor(Math.random()*360);
+    // console.log((hue1 + 200)%360);
+    while(hue2 < (hue1 + 200)%360){
+        hue2 = Math.floor(Math.random()*360);
     }
+    col1 = hue1+",75%,55%";
+    col2 = hue2+",75%,55%";
+
 }
 
-window.onload = function () {
-    page = document.getElementById("page");
-    resize();
+function main() {
+    rndColours();
+    resizeMyCanvas();
+    var canvas = document.getElementById('canvas');
+    ctx  = canvas.getContext('2d');
+    width = ctx.canvas.clientWidth;
+    height = ctx.canvas.clientHeight;
+    wormX=Math.random()*width;
+    wormY=Math.random()*height;
+    ctx.font="12px Courier";
+    ctx.fillStyle = "hsla("+col1+",0.5)";
+
+    if(window.location.hash) {
+        var hash = window.location.hash.substring(1);
+        if(hash == "projects"){
+            colourSwap(1);
+        }else if(hash == "cv"){
+            colourSwap(2);
+        }else if(hash == "contact"){
+            colourSwap(3);
+        }
+    }
+
+    // dothing(ctx);
+    // window.setInterval(function(){dothing()}, 50);
+    // renderMap();
+    window.setInterval(function(){renderMap()}, 1);
+
+    canvas.addEventListener('mousemove', function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+        // ctx.beginPath();
+        // ctx.arc(mousePos.x, mousePos.y, 50, 0, 2 * Math.PI, false);
+        // ctx.fill();
+      }, false);
 }
 
-window.onresize = function (event) {
-    resize();
-};
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
+
+function renderMap(){
+    yMod = Math.random()*90;
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var mSplit = Asciimap.split("\r\n");
+    for(var i=0; i<mSplit.length; i++){
+        ctx.fillText(mSplit[i].substring(mod,mSplit[i].length)+mSplit[i].substring(0,mod)+mSplit[i].substring(mod,mSplit[i].length)+mSplit[i].substring(0,mod)+mSplit[i].substring(mod,mSplit[i].length)+mSplit[i].substring(0,mod)+mSplit[i].substring(mod,mSplit[i].length)+mSplit[i].substring(0,mod),0,-10+i*12+yMod);
+        ctx.fillText(mSplit[i].substring(mSplit[i].length-mod,mSplit[i].length)+mSplit[i].substring(0,mSplit[i].length-mod)+mSplit[i].substring(mSplit[i].length-mod,mSplit[i].length)+mSplit[i].substring(0,mSplit[i].length-mod)+mSplit[i].substring(mSplit[i].length-mod,mSplit[i].length)+mSplit[i].substring(0,mSplit[i].length-mod)+mSplit[i].substring(mSplit[i].length-mod,mSplit[i].length)+mSplit[i].substring(0,mSplit[i].length-mod),0,235+i*12+yMod);
+        ctx.fillText(mSplit[i].substring(mod,mSplit[i].length)+mSplit[i].substring(0,mod)+mSplit[i].substring(mod,mSplit[i].length)+mSplit[i].substring(0,mod)+mSplit[i].substring(mod,mSplit[i].length)+mSplit[i].substring(0,mod)+mSplit[i].substring(mod,mSplit[i].length)+mSplit[i].substring(0,mod),0,480+i*12+yMod);
+        ctx.fillText(mSplit[i].substring(mSplit[i].length-mod,mSplit[i].length)+mSplit[i].substring(0,mSplit[i].length-mod)+mSplit[i].substring(mSplit[i].length-mod,mSplit[i].length)+mSplit[i].substring(0,mSplit[i].length-mod)+mSplit[i].substring(mSplit[i].length-mod,mSplit[i].length)+mSplit[i].substring(0,mSplit[i].length-mod)+mSplit[i].substring(mSplit[i].length-mod,mSplit[i].length)+mSplit[i].substring(0,mSplit[i].length-mod),0,725+i*12+yMod);
+        
+    }
+    // console.log(1-0.3-(mod/100));
+    ctx.fillStyle = "hsla("+col+","+Math.random()/3+")";
+    mod++;
+    if(mod > mSplit[0].length){
+        mod = 0;
+    }
+    if(ctr > Math.floor(Math.random()*100)){
+        ctr = 0;
+        if(colBool == 0){
+            col = col1;
+            colBool = 1;
+        }else{
+            col = col2;
+            colBool = 0;
+        }
+    }
+    ctr++;
+    // moveWorm();
+}
+
+function moveWorm(){
+    if(wormX>width){
+        wormXvel=-speed;
+    }
+    if(wormY>height){
+        wormYvel=-speed;
+    }
+    if(wormX<0){
+        wormXvel=speed;
+    }
+    if(wormY<0){
+        wormYvel=speed;
+    }
+    wormX+=wormXvel;
+    wormY+=wormYvel;
+    ctx.beginPath();
+    ctx.arc(wormX, wormY, 50, 0, 2 * Math.PI, false);
+    ctx.fill();
+
+}
+
+function dothing(){
+    var x = (width-100)*Math.random();
+    var y = height*Math.random();
+
+    // ctx.fillStyle="hsla(255,255,255,"+Math.random()+")";
+
+    ctx.fillStyle="hsla(255,255,255,"+Math.random()+")";
+    ctx.fillText("liam piesley", x, y);
+}
+
+function resizeMyCanvas() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+}
+
+function colourSwap(change){
+    var about = document.getElementById("about");
+    var projects = document.getElementById("projects");
+    var cv = document.getElementById("cv");
+    var contact = document.getElementById("contact");
+    if(change == 0){
+        about.classList.remove("hidden");
+        projects.classList.add("hidden");
+        cv.classList.add("hidden");
+        contact.classList.add("hidden");
+    }else if(change == 1){
+        about.classList.add("hidden");
+        projects.classList.remove("hidden");
+        cv.classList.add("hidden");
+        contact.classList.add("hidden");
+    }else if(change == 2){
+        about.classList.add("hidden");
+        projects.classList.add("hidden");
+        cv.classList.remove("hidden");
+        contact.classList.add("hidden");
+    }else if(change == 3){
+        about.classList.add("hidden");
+        projects.classList.add("hidden");
+        cv.classList.add("hidden");
+        contact.classList.remove("hidden");
+    }
+    if(change == 1 || change == 3){
+        var side = document.getElementById("side");
+        side.style.backgroundColor = "rgb(247, 244, 240)";
+        side.style.color = "black";
+        document.body.style.backgroundColor = "white";
+    }else{
+        var side = document.getElementById("side");
+        side.style.backgroundColor = "black";
+        side.style.color = "rgb(247, 244, 240)";
+        document.body.style.backgroundColor = "black";
+    }
+
+    
+    rndColours();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function setup(){
+}
+
+function draw(){
+}
