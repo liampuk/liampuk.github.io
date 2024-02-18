14a18d4734b9f7778dd70129ee81cae35cb590be
liampuk <liam.piesley@marshmallow.co>
2024-02-18 23:51:06 +0000

    polyglot: add languages, responsive layout

---
+++ polyglot/assets/Page-BjYSiurZ.js
@@ -0,0 +1,198 @@
+import{u as r,a as k,j as a,r as t,S as O}from"./index-BNOmtn0G.js";const v=r.img`
+  height: 40px;
+  width: 60px;
+  border-radius: 4px;
+`,w=[{language:"French",search:"french france benin gabon congo guinea monaco ivory coast niger senegal togo switzerland canada belgium burundi cameroon chad central african republic comoros djibouti equatorial guinea haiti luxembourg madagascar rwanda seychelles vanuatu",words:{hello:"Bonjour",bye:"Au revoir",thanks:"Merci",sorry:"Excusez moi",yes:"Oui",no:"Non"},flag:"https://flagpedia.net/data/flags/w1160/fr.webp",lang:"fr"},{language:"Spanish",search:"spanish spain mexico columbia argentina peru venezuela chile guatemala ecuador bolivia cuba dominican republic honduras paraguay el salvador nicaragua costa rica panama uruguay equatorial guinea puerto rico",words:{hello:"Hola",bye:"Adiós",thanks:"Gracias",sorry:"Lo siento",yes:"Sí",no:"No"},flag:"https://flagpedia.net/data/flags/w1160/es.webp",lang:"es"},{language:"Mandarin",search:"mandarin chinese china taiwan singapore",words:{hello:"Nǐ hǎo",helloOriginal:"你好",bye:"Zài jiàn",byeOriginal:"再见",thanks:"Xièxiè",thanksOriginal:"谢谢",sorry:"Duìbùqǐ",sorryOriginal:"对不起",yes:"Shì de",yesOriginal:"是的",no:"Bù",noOriginal:"不"},flag:"https://flagpedia.net/data/flags/w1160/cn.webp",lang:"zh"},{language:"Italian",search:"italian italy san marino vatican switzerland",words:{hello:"Buongiorno",bye:"Ciao",thanks:"Grazie",sorry:"Scusi",yes:"SÌ",no:"No"},flag:"https://flagpedia.net/data/flags/w1160/it.webp",lang:"it"},{language:"Turkish",search:"turkish turkey türkiye",words:{hello:"Merhaba",bye:"Güle güle",thanks:"Teşekkürler",sorry:"Üzgünüm",yes:"Evet",no:"Hayir"},flag:"https://flagpedia.net/data/flags/w1160/tr.webp",lang:"tr"},{language:"Thai",search:"thai thailand",words:{hello:"S̄wạs̄dī",helloOriginal:"สวัสดี",bye:"Lā k̀xn",byeOriginal:"ลาก่อน",thanks:"K̄hxbkhuṇ",thanksOriginal:"ขอบคุณ",sorry:"K̄hxthos̄ʹ",sorryOriginal:"ขอโทษ",yes:"Chı̀",yesOriginal:"ใช่",no:"Lek̄h thī̀",noOriginal:"เลขที่"},flag:"https://flagpedia.net/data/flags/w1160/th.webp",lang:"th"},{language:"German",search:"german germany belgium austria switzerland luxembourg liechtenstein",words:{hello:"Guten tag",bye:"Auf Wiedersehen",thanks:"Danke",sorry:"Entschuldigung",yes:"Ja",no:"Nein"},flag:"https://flagpedia.net/data/flags/w1160/de.webp",lang:"de"},{language:"Japanese",search:"japanese japan",words:{hello:"Kon'nichiwa",helloOriginal:"こんにちは",bye:"Sayōnara",byeOriginal:"さようなら",thanks:"Arigatō",thanksOriginal:"ありがとう",sorry:"Sumimasen",sorryOriginal:"すみません",yes:"Hai",yesOriginal:"はい",no:"Īe",noOriginal:"いいえ"},flag:"https://flagpedia.net/data/flags/w1160/jp.webp",lang:"ja"},{language:"Greek",search:"greek greece",words:{hello:"Geiá sou",helloOriginal:"Γειά σου",bye:"Antio sas",byeOriginal:"αντιο σας",thanks:"Efcharistó",thanksOriginal:"ευχαριστώ",sorry:"Sygnómi",sorryOriginal:"συγνώμη",yes:"Naí",yesOriginal:"Ναί",no:"Óchi",noOriginal:"όχι"},flag:"https://flagpedia.net/data/flags/w1160/gr.webp",lang:"el-GR"},{language:"Malay",search:"malay malaysian brunei indonesia singapore east timor",words:{hello:"Salaam",bye:"Selamat tinggal",thanks:"Terima kasih",sorry:"Maafkan saya",yes:"Ya",no:"Tidak"},flag:"https://flagpedia.net/data/flags/w1160/my.webp",lang:"ms"},{language:"Portuguese",search:"portuguese portugal angola brazil cape verde timor leste timor-leste equatorial guinea guinea-bissau guinea bissau mozambique sao tome and principe",words:{hello:"Olá",bye:"Adeus",thanks:"Obrigada",sorry:"Com licença",yes:"Sim",no:"Não"},flag:"https://flagpedia.net/data/flags/w1160/pt.webp",lang:"pt"},{language:"Russian",search:"russia russian belarus kazakhstan kyrgyzstan tajikistan",words:{hello:"Privet",helloOriginal:"Привет",bye:"Do svidaniya",byeOriginal:"до свидания",thanks:"Spasibo",thanksOriginal:"Спасибо",sorry:"Proshu proshcheniya",sorryOriginal:"прошу прощения",yes:"Da",yesOriginal:"да",no:"Net",noOriginal:"нет"},flag:"https://flagpedia.net/data/flags/w1160/ru.webp",lang:"ru"},{language:"Arabic",search:"arabic united arab emirates algeria bahrain chad comoros djibouti egypt iraq jordan kuwait lebanon libya mali mauritania morocco oman palestine qatar saudi arabia somalia sudan syria tunisia yemen",words:{hello:"Mrhban",helloOriginal:"مرحبًا",bye:"Mae alsalama",byeOriginal:"مع السلامة",thanks:"Shkran lak",thanksOriginal:"شكرًا لك",sorry:"Aiedhirni",sorryOriginal:"اعذرني",yes:"Naeam",yesOriginal:"نعم",no:"La",noOriginal:"لا"},flag:"https://flagpedia.net/data/flags/w1160/sa.webp",lang:"ar"},{language:"Polish",search:"polish poland",words:{hello:"Witam",bye:"Do widzenia",thanks:"Dziękuję",sorry:"Przepraszam",yes:"Tak",no:"Nie"},flag:"https://flagpedia.net/data/flags/w1160/pl.webp",lang:"pl"},{language:"Dutch",search:"dutch netherlands holland belgium suriname",words:{hello:"Hallo",bye:"Tot ziens",thanks:"Bedankt",sorry:"Pardon",yes:"Ja",no:"Nee"},flag:"https://flagpedia.net/data/flags/w1160/nl.webp",lang:"nl"},{language:"Vietnamese",search:"vietnamese",words:{hello:"Xin chào",bye:"tạm biệt",thanks:"Cảm ơn",sorry:"Xin lỗi",yes:"Đúng",no:"Không"},flag:"https://flagpedia.net/data/flags/w1160/vn.webp",lang:"vi"},{language:"Hindi",search:"hindi indian india",words:{hello:"Namaste",helloOriginal:"नमस्ते",bye:"Alavida",byeOriginal:"अलविदा",thanks:"Dhanyavaad",thanksOriginal:"धन्यवाद",sorry:"Kshama maangana",sorryOriginal:"क्षमा मांगना",yes:"Haan",yesOriginal:"हाँ",no:"Nahin",noOriginal:"नहीं"},flag:"https://flagpedia.net/data/flags/w1160/in.webp",lang:"hi"},{language:"Korean",search:"korean",words:{hello:"Annyeong",helloOriginal:"안녕",bye:"Annyeonghi gaseyo",byeOriginal:"안녕히 가세요",thanks:"Gamsahabnida",thanksOriginal:"감사합니다",sorry:"Joesonghabnida",sorryOriginal:"죄송합니다",yes:"Ye",yesOriginal:"예",no:"Aniyo",noOriginal:"아니요"},flag:"https://flagpedia.net/data/flags/w1160/kr.webp",lang:"ko"},{language:"Croatian",search:"croatian",words:{hello:"Zdravo",bye:"Doviđenja",thanks:"Hvala vam",sorry:"Oprosti",yes:"Da",no:"Ne"},flag:"https://flagpedia.net/data/flags/w1160/hr.webp",lang:"hr"},{language:"Hungarian",search:"hungary hungarian",words:{hello:"Helló",bye:"Viszlát",thanks:"Köszönöm",sorry:"Sajnálom",yes:"Igen",no:"Nem"},flag:"https://flagpedia.net/data/flags/w1160/hu.webp",lang:"hu"}],S=({collapsed:n,languageId:e})=>{const d=w[e];return a.jsxs(L,{$collapsed:n,children:[a.jsx(A,{$collapsed:n,children:a.jsx(E,{children:"Polyglot"})}),a.jsx(C,{$collapsed:n,src:d.flag}),a.jsx(z,{}),a.jsxs(T,{children:["Learn to say ",d.words.hello," in ",w.length," languages"]})]})},z=r.div`
+  height: 150px;
+`,C=r(v)`
+  position: absolute;
+  width: auto;
+  transform: translateX(-50%);
+  transition: all .3s ease;
+  z-index: 1;
+
+  ${({$collapsed:n})=>n?k`
+    left: calc(50% - 96px);
+    top: 20px;
+  `:k`
+    height: 100px;
+    left: 50%;
+    top: 100px;
+  `}
+`,L=r.div`
+  width: 100%;
+  background-color: #f6f5e9;
+  border-bottom: 1px solid #ccc;
+  padding: 16px;
+  position: fixed;
+  transition: .3s ease;
+  display: grid;
+  grid-template-rows: ${({$collapsed:n})=>n?"48px 0 0":"48px 150px auto"};
+  overflow: hidden;
+  gap: ${({$collapsed:n})=>n?"0":"8px"};
+`,T=r.p`
+  font-size: 20px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  text-align: center;
+  height: 16px;
+  font-family: "Annapurna SIL", serif;
+`,A=r.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  transition: .3s;
+  ${({$collapsed:n})=>n&&"margin-left: 48px;"}
+`,E=r.h1`
+  width: fit-content;
+  font-weight: 500;
+  font-size: 48px;
+  font-family: "Annapurna SIL", serif;
+  font-weight: 400;
+  font-style: normal;
+  text-align: center;
+  line-height: 56px;
+`,$=(n,e)=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,ref:e,...n},t.createElement("path",{d:"M11.293 4.707 17.586 11H4v2h13.586l-6.293 6.293 1.414 1.414L21.414 12l-8.707-8.707-1.414 1.414z"})),c=t.forwardRef($),N=({colour:n,language:e})=>{const d=t.useRef(window.speechSynthesis),{romanised:i}=t.useContext(O),s=(h,l)=>{const o=new SpeechSynthesisUtterance(h);o.lang=l,d.current.speak(o)};return a.jsxs(D,{$colour:n,children:[a.jsx(I,{children:e.language}),a.jsxs(H,{children:[a.jsxs(g,{onClick:()=>s(e.words.helloOriginal||e.words.hello,e.lang),children:[a.jsx(p,{children:i?e.words.hello:e.words.helloOriginal||e.words.hello}),a.jsx(c,{}),a.jsx(u,{children:"Hello"})]}),a.jsxs(g,{onClick:()=>s(e.words.byeOriginal||e.words.bye,e.lang),children:[a.jsx(p,{children:i?e.words.bye:e.words.byeOriginal||e.words.bye}),a.jsx(c,{}),a.jsx(u,{children:"Goodbye"})]}),a.jsxs(g,{onClick:()=>s(e.words.thanksOriginal||e.words.thanks,e.lang),children:[a.jsx(p,{children:i?e.words.thanks:e.words.thanksOriginal||e.words.thanks}),a.jsx(c,{}),a.jsx(u,{children:"Thank you"})]}),a.jsxs(g,{onClick:()=>s(e.words.sorryOriginal||e.words.sorry,e.lang),children:[a.jsx(p,{children:i?e.words.sorry:e.words.sorryOriginal||e.words.sorry}),a.jsx(c,{}),a.jsx(u,{children:"Excuse me"})]}),a.jsxs(g,{onClick:()=>s(e.words.yesOriginal||e.words.yes,e.lang),children:[a.jsx(p,{children:i?e.words.yes:e.words.yesOriginal||e.words.yes}),a.jsx(c,{}),a.jsx(u,{children:"Yes"})]}),a.jsxs(g,{onClick:()=>s(e.words.noOriginal||e.words.no,e.lang),children:[a.jsx(p,{children:i?e.words.no:e.words.noOriginal||e.words.no}),a.jsx(c,{}),a.jsx(u,{children:"No"})]})]})]})},H=r.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  cursor: pointer;
+  
+  > :not(:last-child) {
+    border-bottom: 1px solid #ccc;
+  }
+`,g=r.div`
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  padding: 16px;
+  line-height: 24px;
+
+  &:hover {
+    background-color: rgba(0,0,0,0.05);
+  }
+`,p=r.p`
+  text-align: left;
+`,u=r.p`
+  text-align: right;
+`,I=r.h2`
+  font-weight: 400;
+  margin: 0;
+  font-size: 26px;
+  font-family: "Annapurna SIL", serif;
+  margin: 16px 0 8px 16px;
+`,D=r.div`
+  background-color: ${({$colour:n})=>n};
+  padding: 16px;
+  border-bottom: 1px solid #ccc;
+`,F=({filteredLanguages:n,collapsed:e,setScrollPosition:d,setFooterCollapsed:i})=>{const s=t.useRef(null),h=l=>{const o=l.target;d(x=>(o.scrollTop<x?i(!1):i(!0),o.scrollTop))};return t.useEffect(()=>(s.current&&s.current.addEventListener("scroll",h),()=>window.removeEventListener("scroll",h)),[]),a.jsxs(P,{ref:s,$collapsed:e,children:[a.jsx(M,{children:n.map((l,o)=>a.jsx(N,{colour:o%2===0?"#fffef3":"#f6f5e9",language:l},o))}),a.jsxs(R,{children:[a.jsx(j,{href:"https://liamp.uk",target:"_blank",children:"liamp.uk"}),a.jsxs(G,{children:["languages chosen using ",a.jsx(j,{href:"https://www.unwto.org/tourism-data/global-and-regional-tourism-performance",target:"_blank",children:"UN Tourism"})," 2019 rankings"]})]})]})},R=r.div`
+  width: 100%;
+  background-color: #222;
+  padding: 16px;
+  margin-top: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`,G=r.p`
+  color: white;
+  font-size: 12px;
+`,j=r.a`
+  color: white;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`,P=r.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: #f6f5e9;
+  height: calc(100% - ${({$collapsed:n})=>n?"81px":"295px"});
+  margin-top: ${({$collapsed:n})=>n?"81px":"295px"};
+  transition: .3s;
+  position: fixed;
+  overflow: scroll;
+  min-height: 300px;
+`,M=r.div`
+  @media (orientation: landscape) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`,q=({collapsed:n,search:e,setSearch:d})=>{const i=t.useRef(null),{romanised:s,setRomanised:h}=t.useContext(O),l=o=>{o.preventDefault(),i.current&&i.current.blur()};return a.jsx(B,{$collapsed:n,children:a.jsxs(J,{children:[a.jsx(Y,{onClick:()=>h(o=>!o),children:s?"Aa":"字"}),a.jsx(K,{onSubmit:l,children:a.jsx(W,{ref:i,type:"search",placeholder:"Search",onChange:o=>d(o.target.value),value:e})})]})})},K=r.form`
+  margin-left: 16px;
+  margin-right: 16px;
+`,Y=r.div`
+  border: 1px solid #444;
+  color: white;
+  line-height: 50px;
+  margin-left: 16px;
+  font-size: 24px;
+  height: 50px;
+  border-radius: 4px;
+  cursor: pointer;
+  width: 50px;
+  text-align: center;
+  background-color: #333;
+  user-select: none;
+
+  &:active {
+    border: 1px solid #333;
+  }
+`,B=r.div`
+  width: 100%;
+  background-color: #f6f5e9;
+  position: fixed;
+  transition: .3s;
+  animation: popIn ease .6s;
+  bottom: ${({$collapsed:n})=>n?-81:0}px;
+
+  @keyframes popIn {
+    from {bottom: -81px;}
+    to {bottom: 0;}
+  }
+`,J=r.div`
+  padding: 16px;
+  background-color: #222;
+  display: grid;
+  grid-template-columns: 66px auto;
+  border-top: 1px solid #222;
+
+  @media (orientation: landscape) {
+    padding-left: 33.3%;
+    padding-right: 33.3%
+  }
+`,W=r.input`
+  font-size: 24px;
+  border: none;
+  padding: 8px;
+  background-color: #333;
+  color: #eee;
+  text-align: center;
+  height: 32px;
+  border-radius: 4px;
+  border: 1px solid #444;
+  text-transform: capitalize;
+  width: 100%;
+  height: 50px;
+  margin-right: 16px;
+  padding-left: 16px;
+
+  &::-webkit-search-cancel-button {
+    -webkit-appearance: none;
+    height: 20px;
+    width: 20px;
+    background: url("/cross.svg?react");
+    transition: .2s;
+    margin-left: 8px;
+  }
+
+  &::placeholder {
+    color: #aaa
+  }
+
+  &:focus {
+    outline: none;
+  }
+`,U=()=>{const[n,e]=t.useState(0),[d,i]=t.useState(!1),[s,h]=t.useState(0),[l,o]=t.useState(""),x=w.filter(f=>{var y,m;return l===""?!0:(m=(y=f.search)==null?void 0:y.toLowerCase())==null?void 0:m.includes(l.toLowerCase())});t.useEffect(()=>{const f=setInterval(()=>{h(s+1>=w.length?0:s+1)},1500);return()=>clearInterval(f)},[s]);const b=f=>{e(y=>(window.scrollY<y?i(!1):i(!0),window.scrollY))};return t.useEffect(()=>(window.addEventListener("scroll",b),()=>window.removeEventListener("scroll",b)),[]),a.jsxs(X,{children:[a.jsx(S,{collapsed:n!==0,languageId:s}),a.jsx(F,{filteredLanguages:x,collapsed:n!==0,setScrollPosition:e,setFooterCollapsed:i}),a.jsx(q,{search:l,collapsed:d,setSearch:o})]})},X=r.div`
+  background-color: #FFFEF2;
+  overflow: scroll;
+`;export{U as Page};
