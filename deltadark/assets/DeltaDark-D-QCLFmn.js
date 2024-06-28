30144b0618b3bdde8db176fda9eb903b3663b271
liampuk <liam.piesley@marshmallow.co>
2024-06-28 21:04:35 +0100

    deltadark: add delta dark theme

---
+++ deltadark/assets/DeltaDark-D-QCLFmn.js
@@ -0,0 +1,277 @@
+import{r as e,p as r,j as t,c as x}from"./index-DwS9r46k.js";const h=o=>e.createElement("svg",{fill:"#dedede",id:"Capa_1",xmlns:"http://www.w3.org/2000/svg",xmlnsXlink:"http://www.w3.org/1999/xlink",width:"800px",height:"800px",viewBox:"0 0 31.332 31.332",xmlSpace:"preserve",...o},e.createElement("g",null,e.createElement("path",{d:"M23.047,15.266c0.781,0.781,0.781,2.047,0,2.828l-7.381,7.381l-7.379-7.379c-0.781-0.781-0.781-2.046,0-2.828 c0.78-0.781,2.047-0.781,2.827,0l2.552,2.551V8.686c0-1.104,0.896-2,2-2c1.104,0,2,0.896,2,2v9.132l2.553-2.553 C21,14.484,22.268,14.484,23.047,15.266z M31.332,15.666c0,8.639-7.027,15.666-15.666,15.666C7.026,31.332,0,24.305,0,15.666 C0,7.028,7.026,0,15.666,0C24.307,0,31.332,7.028,31.332,15.666z M27.332,15.666C27.332,9.233,22.1,4,15.666,4 C9.233,4,4,9.233,4,15.666C4,22.1,9.233,27.332,15.666,27.332C22.1,27.332,27.332,22.1,27.332,15.666z"}))),u=({display:o})=>t.jsx(t.Fragment,{children:t.jsx("a",{href:"dark.deltaskin",download:!0,children:t.jsxs(C,{$display:o,children:[t.jsx("p",{children:"Download"}),t.jsx(g,{})]})})}),g=r(h)`
+  width: 12px;
+  height: 12px;
+  margin-left: 12px;
+  margin-top: 2px;
+`,C=r.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 150px;
+  height: 60px;
+  z-index: 999;
+  transform: translate(-410px, 350px);
+  border-radius: 16px;
+  padding: 16px;
+  font-size: 14px;
+  cursor: pointer;
+  transition: all 0.3s;
+  border: 1px solid #2e2e2e;
+  color: #dedede;
+  background-color: #282828;
+  opacity: 0;
+
+  ${({$display:o})=>o&&"opacity: 1;"}
+
+  &:after {
+    content: "";
+    position: absolute;
+    left: -1px;
+    top: -1px;
+    width: calc(100% + 2px);
+    height: calc(100% + 2px);
+    border-radius: 16px;
+    border-top: 1px solid #3f3f3f;
+    z-index: 2;
+  }
+
+  &:hover {
+    background-color: #2d2d2d;
+  }
+
+  &:active {
+    background-color: #313131;
+  }
+`,l=o=>e.createElement("svg",{width:390,height:802,viewBox:"0 0 390 802",fill:"none",xmlns:"http://www.w3.org/2000/svg",...o},e.createElement("g",{clipPath:"url(#clip0_48_403)"},e.createElement("path",{d:"M388.432 237.38H383.11C382.647 237.38 382.274 237.753 382.274 238.216V323.859C382.274 324.322 382.647 324.695 383.11 324.695H388.432C388.895 324.695 389.267 324.322 389.267 323.859V238.216C389.267 237.753 388.895 237.38 388.432 237.38Z",fill:"#6C635B"}),e.createElement("path",{d:"M388.479 237.705H383.063C382.626 237.705 382.274 238.057 382.274 238.495V323.579C382.274 324.017 382.626 324.37 383.063 324.37H388.479C388.916 324.37 389.267 324.017 389.267 323.579V238.495C389.267 238.057 388.916 237.705 388.479 237.705Z",fill:"url(#paint0_linear_48_403)"}),e.createElement("path",{d:"M6.89006 200.406H1.56868C1.10568 200.406 0.73291 200.779 0.73291 201.242V252.168C0.73291 252.631 1.10557 253.004 1.56868 253.004H6.89006C7.35305 253.004 7.72583 252.631 7.72583 252.168V201.242C7.72583 200.779 7.35317 200.406 6.89006 200.406Z",fill:"#6C635B"}),e.createElement("path",{d:"M6.93738 200.731H1.5214C1.08456 200.731 0.73291 201.084 0.73291 201.522V251.888C0.73291 252.326 1.08441 252.679 1.5214 252.679H6.93738C7.37418 252.679 7.72583 252.326 7.72583 251.888V201.522C7.72583 201.084 7.37433 200.731 6.93738 200.731Z",fill:"url(#paint1_linear_48_403)"}),e.createElement("path",{d:"M6.89006 143.923H1.56868C1.10568 143.923 0.73291 144.296 0.73291 144.759V174.385C0.73291 174.848 1.10557 175.22 1.56868 175.22H6.89006C7.35305 175.22 7.72583 174.848 7.72583 174.385V144.759C7.72583 144.296 7.35317 143.923 6.89006 143.923Z",fill:"#6C635B"}),e.createElement("path",{d:"M6.93738 144.249H1.5214C1.08456 144.249 0.73291 144.601 0.73291 145.039V174.105C0.73291 174.543 1.08441 174.896 1.5214 174.896H6.93738C7.37418 174.896 7.72583 174.543 7.72583 174.105V145.039C7.72583 144.601 7.37433 144.249 6.93738 144.249Z",fill:"url(#paint2_linear_48_403)"}),e.createElement("path",{d:"M6.89006 271.418H1.56868C1.10568 271.418 0.73291 271.791 0.73291 272.254V323.18C0.73291 323.643 1.10557 324.016 1.56868 324.016H6.89006C7.35305 324.016 7.72583 323.643 7.72583 323.18V272.254C7.72583 271.791 7.35317 271.418 6.89006 271.418Z",fill:"#6C635B"}),e.createElement("path",{d:"M6.93738 271.744H1.5214C1.08456 271.744 0.73291 272.096 0.73291 272.534V322.901C0.73291 323.338 1.08441 323.691 1.5214 323.691H6.93738C7.37418 323.691 7.72583 323.338 7.72583 322.901V272.534C7.72583 272.096 7.37433 271.744 6.93738 271.744Z",fill:"url(#paint3_linear_48_403)"}),e.createElement("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M328.112 0.460815H61.8877C29.1664 0.460815 2.82397 26.8033 2.82397 59.5246V742.476C2.82397 775.197 29.1664 801.539 61.8877 801.539H328.112C360.833 801.539 387.176 775.197 387.176 742.476V59.5246C387.176 26.8033 360.833 0.460815 328.112 0.460815ZM62.8907 14.413H327.109C353.059 14.413 373.95 35.3038 373.95 61.2534V740.747C373.95 766.696 353.059 787.587 327.109 787.587H62.8907C36.9411 787.587 16.0503 766.696 16.0503 740.747V61.2534C16.0503 35.3038 36.9411 14.413 62.8907 14.413Z",fill:"#6C635B"}),e.createElement("g",{filter:"url(#filter0_f_48_403)"},e.createElement("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M327.182 2.76123H62.8184C30.6526 2.76123 4.75732 28.6565 4.75732 60.8223V741.178C4.75732 773.343 30.6526 799.239 62.8184 799.239H327.182C359.348 799.239 385.243 773.343 385.243 741.178V60.8223C385.243 28.6565 359.348 2.76123 327.182 2.76123ZM59.9939 10.7783H328.864C355.27 10.7783 376.529 31.8585 376.529 58.0432V743.693C376.529 769.878 355.27 790.958 328.864 790.958H59.9939C33.5875 790.958 12.3289 769.878 12.3289 743.693V58.0432C12.3289 31.8585 33.5875 10.7783 59.9939 10.7783Z",fill:"#DAD5C5"})),e.createElement("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M307.164 14.413V0.461426H312.063V14.413H307.164ZM16.0503 77.5532H2.82471V82.4517H16.0503V77.5532ZM16.0503 719.548H2.82471V724.447H16.0503V719.548ZM77.936 787.587V801.538H82.8345V787.587H77.936ZM373.95 724.447V719.548H387.176V724.447H373.95ZM373.95 82.4517V77.5532H387.176V82.4517H373.95Z",fill:"#3D342C",fillOpacity:.295364}),e.createElement("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M326.282 5.11304H63.7178C32.6506 5.11304 7.63989 30.1238 7.63989 61.191V740.809C7.63989 771.876 32.6506 796.887 63.7178 796.887H326.282C357.349 796.887 382.36 771.876 382.36 740.809V61.191C382.36 30.1238 357.349 5.11304 326.282 5.11304ZM62.8907 14.413H327.109C353.059 14.413 373.95 35.3038 373.95 61.2534V740.747C373.95 766.696 353.059 787.587 327.109 787.587H62.8907C36.9411 787.587 16.0503 766.696 16.0503 740.747V61.2534C16.0503 35.3038 36.9411 14.413 62.8907 14.413Z",fill:"#151515"}),e.createElement("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M324.097 8.68787H65.9034C36.2231 8.68787 12.3289 32.5821 12.3289 62.2624V739.738C12.3289 769.418 36.2231 793.312 65.9034 793.312H324.097C353.777 793.312 377.671 769.418 377.671 739.738V62.2624C377.671 32.5821 353.777 8.68787 324.097 8.68787ZM62.8907 14.413H327.109C353.059 14.413 373.95 35.3038 373.95 61.2534V740.747C373.95 766.696 353.059 787.587 327.109 787.587H62.8907C36.9411 787.587 16.0503 766.696 16.0503 740.747V61.2534C16.0503 35.3038 36.9411 14.413 62.8907 14.413Z",fill:"black"}),e.createElement("path",{d:"M154.819 5.11304C158.934 5.11304 158.828 6.09462 159.68 7.20841C160.301 8.02077 161.102 8.27954 162.677 8.27954H227.323C228.898 8.27954 229.697 8.02075 230.318 7.20841C231.171 6.09462 231.066 5.11304 235.181 5.11304H154.819Z",fill:"url(#paint4_linear_48_403)"})),e.createElement("defs",null,e.createElement("filter",{id:"filter0_f_48_403",x:3.41733,y:1.42124,width:383.165,height:799.158,filterUnits:"userSpaceOnUse",colorInterpolationFilters:"sRGB"},e.createElement("feFlood",{floodOpacity:0,result:"BackgroundImageFix"}),e.createElement("feBlend",{mode:"normal",in:"SourceGraphic",in2:"BackgroundImageFix",result:"shape"}),e.createElement("feGaussianBlur",{stdDeviation:.669995,result:"effect1_foregroundBlur_48_403"})),e.createElement("linearGradient",{id:"paint0_linear_48_403",x1:389.267,y1:237.836,x2:389.267,y2:324.145,gradientUnits:"userSpaceOnUse"},e.createElement("stop",{stopColor:"#DAD5C5"}),e.createElement("stop",{offset:.124105,stopColor:"#BCB6A8"}),e.createElement("stop",{offset:.764916,stopColor:"#9B9388"}),e.createElement("stop",{offset:.985317,stopColor:"#80786E"}),e.createElement("stop",{offset:1,stopColor:"#B9B3A5"})),e.createElement("linearGradient",{id:"paint1_linear_48_403",x1:40.7567,y1:200.862,x2:40.7567,y2:252.516,gradientUnits:"userSpaceOnUse"},e.createElement("stop",{stopColor:"#DAD5C5"}),e.createElement("stop",{offset:.124105,stopColor:"#BCB6A8"}),e.createElement("stop",{offset:.764916,stopColor:"#9B9388"}),e.createElement("stop",{offset:.985317,stopColor:"#80786E"}),e.createElement("stop",{offset:1,stopColor:"#B9B3A5"})),e.createElement("linearGradient",{id:"paint2_linear_48_403",x1:40.7567,y1:144.38,x2:40.7567,y2:174.736,gradientUnits:"userSpaceOnUse"},e.createElement("stop",{stopColor:"#DAD5C5"}),e.createElement("stop",{offset:.124105,stopColor:"#BCB6A8"}),e.createElement("stop",{offset:.764916,stopColor:"#9B9388"}),e.createElement("stop",{offset:.985317,stopColor:"#80786E"}),e.createElement("stop",{offset:1,stopColor:"#B9B3A5"})),e.createElement("linearGradient",{id:"paint3_linear_48_403",x1:40.7567,y1:271.875,x2:40.7567,y2:323.528,gradientUnits:"userSpaceOnUse"},e.createElement("stop",{stopColor:"#DAD5C5"}),e.createElement("stop",{offset:.124105,stopColor:"#BCB6A8"}),e.createElement("stop",{offset:.764916,stopColor:"#9B9388"}),e.createElement("stop",{offset:.985317,stopColor:"#80786E"}),e.createElement("stop",{offset:1,stopColor:"#B9B3A5"})),e.createElement("linearGradient",{id:"paint4_linear_48_403",x1:162.735,y1:5.11304,x2:162.735,y2:8.27954,gradientUnits:"userSpaceOnUse"},e.createElement("stop",null),e.createElement("stop",{offset:1,stopColor:"#212121"})),e.createElement("clipPath",{id:"clip0_48_403"},e.createElement("rect",{width:390,height:802,fill:"white"})))),w=({initial:o,fastAnimate:n,top:i,setOnTop:a})=>t.jsx(b,{$initial:o,$fastAnimate:n,$top:i,onMouseEnter:a,children:t.jsxs(E,{children:[t.jsx(j,{width:"775",height:"360",autoPlay:!0,loop:!0,muted:!0,children:t.jsx("source",{src:"landscape.mp4",type:"video/mp4"})}),t.jsx(v,{})]})}),b=r.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  height: 390px;
+  width: 802px;
+  transform: translateX(-120px) scale(0.9);
+  rotate: -8deg;
+  transition: all ease ${({$fastAnimate:o})=>o?".3":"1.3"}s;
+  ${({$top:o})=>o&&"z-index: 999;"}
+
+  &:hover {
+    rotate: 0deg;
+    transform: translateX(-200px) scale(1);
+  }
+
+  ${({$initial:o})=>o&&x`
+      transform: translateX(-400px) scale(1.2);
+      filter: blur(10px);
+      rotate: -12deg;
+    `}
+`,E=r.div`
+  height: 390px;
+  width: 802px;
+  overflow: hidden;
+`,v=r(l)`
+  position: absolute;
+  width: 390px;
+  transform: rotate(-90deg);
+  top: -208px;
+  left: 208px;
+`,j=r.video`
+  position: absolute;
+  left: 15px;
+  top: 13px;
+  width: 776px;
+  border-radius: 48px;
+  -webkit-filter: drop-shadow(12px 12px 6px rgba(0, 0, 0, 0.4));
+  filter: drop-shadow(18px 18px 16px rgba(0, 0, 0, 0.4));
+`,V=()=>t.jsx(t.Fragment,{children:t.jsx("a",{href:"dark.deltaskin",download:!0,children:t.jsxs(y,{children:[t.jsx("p",{children:"Download"}),t.jsx(H,{})]})})}),H=r(h)`
+  width: 12px;
+  height: 12px;
+  margin-left: 12px;
+  margin-top: 2px;
+`,y=r.button`
+  margin-bottom: 64px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 60px;
+  z-index: 999;
+  border-radius: 16px;
+  padding: 16px;
+  font-size: 14px;
+  cursor: pointer;
+  transition: all 0.3s;
+  border: 1px solid #2e2e2e;
+  color: #dedede;
+  background-color: #282828;
+
+  &:hover {
+    background-color: #2d2d2d;
+  }
+
+  &:active {
+    background-color: #313131;
+  }
+`,_=()=>t.jsx($,{children:t.jsx(B,{children:t.jsxs(k,{children:[t.jsx(M,{width:"775",height:"360",autoPlay:!0,loop:!0,muted:!0,children:t.jsx("source",{src:"landscape.mp4",type:"video/mp4"})}),t.jsx(D,{})]})})}),$=r.div`
+  width: 100vw;
+  height: 802px;
+  position: static;
+  margin-top: 64px;
+  margin-bottom: 64px;
+`,B=r.div`
+  position: relative;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: 390px;
+  height: 802px;
+`,k=r.div`
+  width: 390px;
+  height: 802px;
+`,D=r(l)`
+  position: absolute;
+  width: 390px;
+`,M=r.video`
+  rotate: 90deg;
+  position: absolute;
+  left: 16px;
+  top: 13px;
+  width: 776px;
+  left: -193px;
+  top: 222px;
+  border-radius: 48px;
+  -webkit-filter: drop-shadow(12px 12px 6px rgba(0, 0, 0, 0.4));
+  filter: drop-shadow(18px 18px 16px rgba(0, 0, 0, 0.4));
+`,Z=()=>t.jsx(A,{children:t.jsx(S,{children:t.jsxs(P,{children:[t.jsx(R,{width:"360",height:"775",autoPlay:!0,loop:!0,muted:!0,children:t.jsx("source",{src:"portrait.mp4",type:"video/mp4"})}),t.jsx(z,{})]})})}),A=r.div`
+  width: 100vw;
+  height: 802px;
+  position: static;
+  margin-top: 32px;
+`,S=r.div`
+  position: relative;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: 390px;
+  height: 802px;
+`,P=r.div`
+  width: 390px;
+  height: 802px;
+`,z=r(l)`
+  position: absolute;
+  width: 390px;
+`,R=r.video`
+  position: absolute;
+  left: 16px;
+  top: 13px;
+  width: 358px;
+  border-radius: 48px;
+  -webkit-filter: drop-shadow(12px 12px 6px rgba(0, 0, 0, 0.4));
+  filter: drop-shadow(18px 18px 16px rgba(0, 0, 0, 0.4));
+`,U=()=>t.jsxs(T,{children:[t.jsx(G,{children:"Delta Dark Theme"}),t.jsxs(d,{$margin:"16px 0 0 0",children:["A Nintendo DS™ theme for the"," ",t.jsx(O,{href:"https://apps.apple.com/us/app/delta-game-emulator/id1048524688",target:"_blank",children:"Delta game emulator."})]}),t.jsx(d,{$margin:"8px 0",children:"Designed for dynamic island iPhones with large shoulder buttons that extend into the top screen in portrait mode for easy drifting, and a large screen in landscape mode for easy viewing."}),t.jsx(Z,{}),t.jsx(_,{}),t.jsx(V,{})]}),T=r.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`,O=r.a`
+  color: #0fffc4;
+
+  &:hover {
+    background-color: #0fffc4;
+    color: #1f1f1f;
+  }
+`,G=r.span`
+  font-family: "Vollkorn", serif;
+  font-size: 56px;
+  color: #f1f1f1;
+  margin-top: 64px;
+  text-align: center;
+  line-height: 56px;
+`,d=r.p`
+  color: #ccc;
+  font-size: 14px;
+  width: 340px;
+  margin: ${({$margin:o})=>o&&o};
+`,F=({initial:o,fastAnimate:n,top:i,setOnTop:a})=>t.jsx(I,{$initial:o,$fastAnimate:n,$top:i,onMouseEnter:a,children:t.jsxs(X,{children:[t.jsx(L,{width:"360",height:"775",autoPlay:!0,loop:!0,muted:!0,children:t.jsx("source",{src:"portrait.mp4",type:"video/mp4"})}),t.jsx(W,{})]})}),I=r.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: 390px;
+  height: 802px;
+  rotate: 8deg;
+  transition: all ease 1s;
+  transform: translateX(280px) scale(0.9);
+  transition: all ease ${({$fastAnimate:o})=>o?".3":"1.3"}s;
+  ${({$top:o})=>o&&"z-index: 999;"}
+
+  &:hover {
+    rotate: 0deg;
+    transform: translateX(200px) scale(1);
+  }
+
+  ${({$initial:o})=>o&&x`
+      transform: translateX(400px) scale(1.2);
+      filter: blur(10px);
+      rotate: 12deg;
+    `}
+`,X=r.div`
+  width: 390px;
+  height: 802px;
+`,W=r(l)`
+  position: absolute;
+  width: 390px;
+`,L=r.video`
+  position: absolute;
+  left: 16px;
+  top: 13px;
+  width: 358px;
+  border-radius: 48px;
+  -webkit-filter: drop-shadow(12px 12px 6px rgba(0, 0, 0, 0.4));
+  filter: drop-shadow(18px 18px 16px rgba(0, 0, 0, 0.4));
+`,N=({display:o})=>t.jsxs(K,{$display:o,children:[t.jsx(J,{children:"Delta Dark Theme"}),t.jsxs(c,{children:["A Nintendo DS™ theme for the"," ",t.jsx(q,{href:"https://apps.apple.com/us/app/delta-game-emulator/id1048524688",target:"_blank",children:"Delta game emulator."})]}),t.jsx(c,{$margin:"8px 0",children:"Designed for dynamic island iPhones with large shoulder buttons that extend into the top screen in portrait mode for easy drifting, and a large screen in landscape mode for easy viewing."})]}),q=r.a`
+  color: #0fffc4;
+
+  &:hover {
+    background-color: #0fffc4;
+    color: #1f1f1f;
+  }
+`,J=r.span`
+  font-family: "Vollkorn", serif;
+  font-size: 42px;
+`,c=r.p`
+  color: #ccc;
+  font-size: 14px;
+  width: 340px;
+  margin: ${({$margin:o})=>o&&o};
+`,K=r.div`
+  position: absolute;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 500px;
+  height: 100px;
+  z-index: 999;
+  transform: translate(-258px, -320px);
+  color: #f1f1f1;
+  border-radius: 16px;
+  padding: 16px;
+  transition: all 0.5s;
+  opacity: 0;
+
+  ${({$display:o})=>o&&"opacity: 1;"}
+`,ee=()=>{const[o,n]=e.useState(!0),[i,a]=e.useState(!1),[s,p]=e.useState(!0);return e.useEffect(()=>{const f=setTimeout(()=>n(!1),1),m=setTimeout(()=>a(!0),1e3);return()=>{clearTimeout(f),clearTimeout(m)}},[]),window.innerWidth>window.innerHeight?t.jsxs(t.Fragment,{children:[t.jsx(w,{initial:o,fastAnimate:i,top:s,setOnTop:()=>p(!0)}),t.jsx(F,{initial:o,fastAnimate:i,top:!s,setOnTop:()=>p(!1)}),t.jsx(u,{display:i}),t.jsx(N,{display:i})]}):t.jsx(U,{})};export{ee as DeltaDark};
